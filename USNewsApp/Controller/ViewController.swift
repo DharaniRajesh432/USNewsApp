9fe8d9b36510df59b3ff507be7e9190d079d34f0
Rajesh <Rajesh.iosdev432@gmail.com>
2022-11-24 20:05:08 +0530

    Navigation added

---
+++ USNewsApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  USNewsApp
+//
+//  Created by dharani rajesh on 24/11/22.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
